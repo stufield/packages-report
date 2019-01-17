1284d3add74495a1886918d2439dcc32156eca2b
Stu Field <sfield@somalogic.com>
2019-01-17 08:44:52 -0600

    Minor pipe edit to make_clean.R

--- R/make-clean.R
+++ R/make-clean.R
@@ -1,7 +1,9 @@
 
 dirs <- c(here::here("data"), here::here("figs")) %>%
   purrr::keep(fs::dir_exists)
-purrr::map(dirs, fs::dir_ls) %>%
+#dirs
+dirs %>%
+  purrr::map(fs::dir_ls) %>%
   purrr::flatten_chr() %>%
   unname() %>%
   fs::file_delete()
